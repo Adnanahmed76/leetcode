6634dfb5e342d5b6ac9e56059c9587ee1243e79e
adnan ahmed <adnanahmed504423@gmail.com>
2025-08-08 00:50:27 +0530

    valid parenthises

--- validparathiesis.dart
+++ validparathiesis.dart
@@ -1,3 +1,24 @@
-void main9{
-  
+void main(){
+print(isvalid("()"));
+print(isvalid(""))
+}
+bool isvalid(String s){
+  final Map<String,String> parenthieses={
+    ')':'(',
+     '}':'{',
+     ']':'['
+  };
+  final List<String> stack=[];
+  for(String char in s.split('')){
+    if(parenthieses.containsKey(char)){
+      if(stack.isEmpty|| stack.last!=parenthieses[char]){
+        return false;
+      }
+      stack.removeLast();
+    }else{
+      stack.add(char);
+    }
+
+  }
+  return stack.isEmpty;
 }
