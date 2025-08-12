2010c6ae1d7fdb1e5374f83c33406a4a7f06d4ec
adnan ahmed <adnanahmed504423@gmail.com>
2025-08-13 01:38:55 +0530

    ternary opeartor

--- mergetwosting.dart
+++ mergetwosting.dart
@@ -1,7 +1,63 @@
+import 'dart:collection';
+import 'dart:io';
 void main(){
-Mergetwosting();
+//example 1
+ListNode? list1=createLinkedList([12,34,25,89]);
+ListNode? list2=createLinkedList([2,6,2,4,6,]);
+
+ListNode? merged=Mergetwosting(list1, list2);
+
+
+//example 2
+
+
+}
+class ListNode {
+int val;
+ListNode? next;
+ListNode(this.val,[this.next]);
 }
 
-class Mergetwosting {
+ListNode? Mergetwosting(ListNode? list1, ListNode? list2){
+  ListNode dummy=ListNode(0);
+  ListNode current=dummy;
+while(list1!=null && list2!=null){
+  if(list1.val<=list2.val){
+    current.next=list1;
+    list1=list1.next;
 
+  }else{
+    current.next=list2;
+    list2=list2.next;
+
+  }
+  current=current.next!;
+}
+//attach remaining nodes
+current.next=(list1!=null)? list1:list2;
+return dummy.next;
+}
+
+//create linked list from array
+ListNode? createLinkedList(List<int> value){
+  if(value.isEmpty)return null;
+  ListNode head=ListNode(value[0]);
+  ListNode current=head;
+  for(int i=1;i<value.length;i++){
+    current.next=ListNode(value[i]);
+    current=current.next!;
+
+  }
+  return head;
+}
+
+//print linked list in array format
+void printLinkedList(ListNode? head){
+  List<int> value=[];
+  while(head!=null){
+    value.add(head.val);
+    head=head.next;
+
+  }
+  print(value);
 }
--- recursion_func.dart
+++ recursion_func.dart
@@ -1,14 +1,27 @@
-void main(){
-  var sum=addNumbers(5);
-  print("total number of sum is $sum");
-}
+// void main(){
+//   var sum=addNumbers(5);
+//   print("total number of sum is $sum");
+// }
 
-int addNumbers(var n){
-  print("value pf n is $n");
-  //termination condition 
-  if(n==1){
-    return 1;
-  }else{
-    return n+addNumbers(n-1);
+// int addNumbers(var n){
+//   print("value pf n is $n");
+//   //termination condition 
+//   if(n==1){
+//     return 1;
+//   }else{
+//     //5 
+//     return n+addNumbers(n-1);
+//   }
+// }
+
+//ternary operator for pizza avalible logic based
+
+import 'dart:math';
+
+void main(){
+  bool pizzaavaliable=false;
+  String pizza="Chese pizza";
+  String burger="Veg Burger";
+  String food=(pizzaavaliable)?pizza: burger;
+  print(food);
   }
-}
